fbec15a201ba0ca8a7e4d1b784585c2a82885094
Maegical <147511652+Maegical@users.noreply.github.com>
2023-10-17 08:07:51 -0400

    week 3

---
+++ Week3a.js
@@ -0,0 +1,9 @@
+console.log("Hello! Week 3 A reporting!");
+
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.font="30px Arial";
+c.fillText
