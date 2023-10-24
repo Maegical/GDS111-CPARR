6df091cba3910ef04a3c86a17273eb3a585b3511
Maegical <147511652+Maegical@users.noreply.github.com>
2023-10-24 09:09:35 -0400

    week4

---
+++ week4a.js
@@ -0,0 +1,9 @@
+var wideScope = 7;
+if (wideScope == 7) { 
+        let localScope = 10;
+        console.log("localScope outsidenside if-block = " + localScope);
+        console.log("wideScope outnside if-block = " + wideScope);
+}
+
+/* console.log("localScope outsidenside if-block = " + localScope); */
+console.log("wideScope outnside if-block = " + wideScope);
