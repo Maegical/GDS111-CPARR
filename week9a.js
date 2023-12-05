2a96ff48c1bea697b700da3f11b7ab2b35b89ca1
Maegical <147511652+Maegical@users.noreply.github.com>
2023-12-05 10:58:39 -0500

    week8

---
+++ week9a.js
@@ -0,0 +1,22 @@
+let start = Number(prompt("Type start number: "));
+let end =  Number(prompt("Type end number: "));
+
+for(i=start; i<end; i++) {
+    if ( ( i % 3 == 0) && (i % 5 == 0)) { 
+        console.log("Fizz Buzz");
+      //  alert("Fizz Buzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+       // alert("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+       // alert("Buzz");
+    } else {
+        console.log(i);
+       // alert(i);
+    }
+}
+
+const arrayString= [i];
+alert(i);
+
---
+++ week9ab.js
@@ -0,0 +1,27 @@
+//const arrayString= [i];
+//alert(i);
+
+
+let start = Number(prompt("Type start number: "));
+let end =  Number(prompt("Type end number: "));
+let fun = []
+
+
+for(i=start; i<end; i++) {
+    if ( ( i % 3 == 0) && (i % 5 == 0)) { 
+        fun.push(String("Fizz Buzz"));
+      //  alert("Fizz Buzz");
+    } else if (i % 3 == 0) {
+        fun.push(String("Fizz"));
+       // alert("Fizz");
+    } else if (i % 5 == 0) {
+        fun.push(String("Buzz"));
+       // alert("Buzz");
+    } else {
+        fun.push(String(i));
+       // alert(i);
+    }
+} 
+
+
+console.log (fun);
